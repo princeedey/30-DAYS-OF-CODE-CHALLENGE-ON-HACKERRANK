f3be38811c1240941c33349749d89723ceabcd54
Princee Dey <66840753+princeedey@users.noreply.github.com>
2020-10-19 20:25:40 +0530

    Add files via upload

---
+++ Day_16.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day_16 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try
+        {
+            System.out.println(Integer.parseInt(S));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Bad String");
+        }
+    }
+}
